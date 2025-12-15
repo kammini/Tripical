5a0d7b91757763e7a69d548aab0a64bd666b11a6
kammini <164534632+kammini@users.noreply.github.com>
2025-12-15 19:28:50 +0100

    ..#

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,23 @@
 import Form from 'next/form'
 
-export default function details() {
+export default function contact() {
   return(
     <main>
       <section className="bg-[url('/surfing.jpg')] bg-cover bg-center h-50 flex justify-center items-center">
-        <span className="text-white text-4xl font-medium drop-shadow-lg">Contact</span>
+        <span className="text-white text-4xl font-medium drop-shadow-lg">Contact Us</span>
       </section>
 
       <section>
-        <Form action="/details">
-          <label></label>
+        <Form action="/details" className='flex'>
+          <input id="name" type="text" placeholder='name'/>
+
+          <input id="email" type="email"placeholder='E-Mail'/>
+
+          <input id="phone" type="tel" inputMode='numeric' placeholder="0812 3456 7890"/>
+
+          <textarea id="message" placeholder='Description'></textarea>
+
+          <button type='submit'>Send</button>
         </Form>
       </section>
     </main>
