fef9d5562455a8345947de4ccf6c6b1b5c471530
kammini <164534632+kammini@users.noreply.github.com>
2025-12-15 23:29:41 +0100

    improve form design

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,16 +8,32 @@ export default function contact() {
       </section>
 
       <section>
-        <Form action="/details" className='flex'>
-          <input id="name" type="text" placeholder='name'/>
+        <Form action="/details" className='flex flex-row m-5 gap-10'>
+          <div className='flex flex-col gap-10'>
+            <input id="name" 
+            type="text" 
+            placeholder='name' 
+            className='border border-stone-500 rounded-xs py-2 px-2'/>
 
-          <input id="email" type="email"placeholder='E-Mail'/>
+            <input id="email" 
+            type="email"
+            placeholder='E-Mail'
+            className='border border-stone-500 rounded-xs py-2 px-2'/>
 
-          <input id="phone" type="tel" inputMode='numeric' placeholder="0812 3456 7890"/>
+            <input id="phone" 
+            type="tel" 
+            inputMode='numeric' 
+            placeholder="0812 3456 7890"
+            className='border border-stone-500 rounded-xs py-2 px-2'/>
+          </div>
 
-          <textarea id="message" placeholder='Description'></textarea>
+          <textarea id="message" 
+          placeholder='Share with us your question or your plan'
+          className='resize-none border border-stone-500 rounded-xs py-2 px-2 w-11/12'></textarea>
 
-          <button type='submit'>Send</button>
+          <button type='submit'
+          className='border rounded-sm bg-green-400 text-white py-1 px-3 drop-shadow-sm'
+          >Send</button>
         </Form>
       </section>
     </main>
