af40bac0c35b30b24e2da2f41b8c6b0abf734fe0
kammini <164534632+kammini@users.noreply.github.com>
2026-02-26 17:42:30 +0700

    add api

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -28,7 +28,7 @@ const ContactForm = () => {
     setServerMessage('');
     setIsSuccess(false);
     try {
-      const response = await fetch('http://localhost:5001/send-email', {
+      const response = await fetch('/api/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
